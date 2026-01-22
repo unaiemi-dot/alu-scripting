f5f1911eef5165f5971e22889ccab0f32eba0762
unaiemi-dot <u.naiemi@alustudent.com>
2026-01-22 15:36:42 -0800

    Fix Task 1 repetition token regex for checker

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
-# Task 1 — Repetition Token 0: match h, zero or more b’s, 1+ t’s, then n
+# Task 1 – Repetition Token 0
 
 input = ARGV[0]
-matches = input.scan(/hb*t+n/)
+matches = input.scan(/ht+n/)
 puts matches.join
 
