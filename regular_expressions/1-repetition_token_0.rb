34fa5314cd2b03bf492f5f98c7124570f57ded79
unaiemi-dot <u.naiemi@alustudent.com>
2026-01-22 15:34:30 -0800

    Add Task 1 repetition token 0 regex script

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -1,9 +1,7 @@
 #!/usr/bin/env ruby
-# Task 1 – repetition token 0
-# Matches "h" followed by zero or more "b", then one or more "t", then "n"
+# Task 1 — Repetition Token 0: match h, zero or more b’s, 1+ t’s, then n
 
 input = ARGV[0]
 matches = input.scan(/hb*t+n/)
 puts matches.join
-
 
