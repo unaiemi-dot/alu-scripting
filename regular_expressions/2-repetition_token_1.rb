df051c17855535324b344d18f0b0a576e6fab9e5
unaiemi-dot <u.naiemi@alustudent.com>
2026-01-22 14:53:10 -0800

    Fix Task 2 repetition token 1 regex

--- regular_expressions/2-repetition_token_1.rb
+++ regular_expressions/2-repetition_token_1.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 # Task 2 – repetition token 1
-# Matches "h" followed by exactly one "b" and exactly one "t", then "n"
 
 input = ARGV[0]
-matches = input.scan(/hbt?n/)
+matches = input.scan(/hb*t+n/)
 puts matches.join
+
